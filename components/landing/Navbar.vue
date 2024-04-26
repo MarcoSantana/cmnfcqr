0feea3325438aeb5fcf1f7af82df17435b3346fd
MarcoSantana <marco.santana@gmail.com>
2024-04-26 13:20:31 -0600

    Changed app name in navbar

--- components/landing/Navbar.vue
+++ components/landing/Navbar.vue
@@ -29,8 +29,8 @@ const open = ref(false);
   <LandingContainer>
     <header class="flex flex-col lg:flex-row justify-between items-center my-5 pa-5">
       <div class="flex w-full lg:w-auto items-center justify-between">
-        <a href="/" class="text-lg"><span class="font-bold text-slate-800">Nuxt</span><span
-            class="text-slate-500">ship</span>
+        <a href="/" class="text-lg"><span class="font-bold text-slate-800">QR</span><span
+            class="text-slate-500">Lister</span>
         </a>
         <div class="block lg:hidden">
           <button @click="open = !open" class="text-gray-800">
