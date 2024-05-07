50975668714741cbcc7ef143c84de4418fee9eb8
MarcoSantana <marco.santana@gmail.com>
2024-05-06 19:55:42 -0600

    Updated URL to match production server

--- server/api/users/[id].ts
+++ server/api/users/[id].ts
@@ -2,11 +2,9 @@ import QRCode from 'qrcode';
 export default defineEventHandler((event) => {
 
   const id = getRouterParam(event, 'id')
-  // Declare a constant for the url
-  const url = `https://cmnfc.org/users/${id}`
 
+  const url = `https://main--cmnfc.netlify.app/users/${id}`
 
-  // Return the url as a qr code
   return QRCode.toString(url, {
     errorCorrectionLevel: 'H',
     type: 'svg'
